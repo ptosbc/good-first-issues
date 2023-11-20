76b243a35e0f312a9fbbd4d3f005836e01a7c758
PTOS B. C <ptosbc@gmail.com>
2023-11-21 00:50:27 +0530

    add website code (#1)
    * add website code
    
    * update readme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,103 @@
-import Image from 'next/image'
+import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+type goodFirstIssueLabelType = {
+  id: number;
+  name: string;
+};
+
+type goodFirstIssueType = {
+  id: number;
+  html_url: string;
+  labels: goodFirstIssueLabelType[];
+  title: string;
+  created_at: string;
+};
+
+async function getGoodFirstIssuesData() {
+  const res = await fetch(
+    "https://api.github.com/search/issues?q=is%3Aissue+label%3A%22good+first+issue%22+state:open+no%3Aassignee&sort=opened&order=desc&per_page=10&page=1",
+    {
+      next: {
+        revalidate: 600,
+      },
+    }
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch good first issues data from GitHub API");
+  }
+
+  const data = await res.json();
+
+  return data.items;
+}
+
+export default async function Home() {
+  const goodFirstIssuesData = await getGoodFirstIssuesData();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-12">
+      <h1 className="mb-3 text-2xl md:text-4xl font-bold">Good First Issues</h1>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+      <p className="mb-5 text-sm font-bold">Find latest good first issues.</p>
+
+      {goodFirstIssuesData.map((goodFirstIssue: goodFirstIssueType) => (
+        <Link
+          key={goodFirstIssue.id}
+          href={goodFirstIssue.html_url}
+          className="block max-w-xl md:max-w-3xl w-full p-6 mb-5 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100"
+        >
+          <h5 className="mb-2 text-base md:text-2xl font-bold tracking-tight text-gray-900">
+            {goodFirstIssue.title}
+          </h5>
+          <p className="mb-2 font-sm text-gray-700">
+            Repository:{" "}
+            <span className="font-bold">
+              {goodFirstIssue.html_url
+                .replace("https://github.com/", "")
+                .replace("/issues/", " #")}
+            </span>
+          </p>
+
+          <p className="mb-2 font-sm text-gray-700">
+            Created At:{" "}
+            <span className="font-bold">
+              {new Date(goodFirstIssue.created_at).toString()}
+            </span>
+          </p>
+
+          <div className="flex flex-wrap gap-2">
+            {goodFirstIssue.labels.map((label: goodFirstIssueLabelType) => (
+              <div
+                key={label.id}
+                className="center relative inline-block select-none whitespace-nowrap rounded-lg bg-blue-500 py-1 md:py-2 px-3.5 align-baseline font-sans text-xs font-bold uppercase leading-none text-white"
+              >
+                <div className="mt-px">{label.name}</div>
+              </div>
+            ))}
+          </div>
+        </Link>
+      ))}
+
+      <h5 className="text-base">
+        Made with{" "}
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
+          className="inline-block mx-1"
+          alt="love"
+          src="/red-heart.png"
+          width={25}
+          height={25}
           priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        />{" "}
+        by{" "}
+        <Link
+          className="inline-block text-blue-500 mx-1"
+          href="https://github.com/ptosbc"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          PTOS B. C.
+        </Link>
+      </h5>
     </main>
-  )
+  );
 }
